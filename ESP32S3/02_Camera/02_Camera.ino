bf4cdd473e11383b6fc6d48f29bb1a491f34b808
Jiaqi Wang <139347307+myry07@users.noreply.github.com>
2024-11-30 01:58:48 +0800

    Update 02_Camera.ino

--- ESP32S3/02_Camera/02_Camera.ino
+++ ESP32S3/02_Camera/02_Camera.ino
@@ -51,7 +51,7 @@ void cameraInit() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000;
+  config.xclk_freq_hz = 10000000; // 10MHz 其实比 20MHz更快
   config.frame_size = FRAMESIZE_QQVGA;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
